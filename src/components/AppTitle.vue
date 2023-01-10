ee85065c81358cdc72376f3040edcf40ebf38edf
Fededada23 <dadamofederico@gmail.com>
2023-01-10 14:04:29 +0100

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "AppTitle",
+    data(){
+        return{
+            hello: 'la mia prima volta con VITE '
+        }
+    },            
+}
+</script>
+<template lang="">
+    <div>
+        {{hello}}
+    </div>
+</template>
+<style lang="">
+    
+</style>
